b021dc94713e605ce8b49733218b0841f4087432
Jakub <jakubbaransky3@gmail.com>
2024-01-31 15:35:49 +0100

    som aky som

--- src/HAHA.java
+++ src/HAHA.java
@@ -1,11 +1,6 @@
 public class HAHA {
     public static void main(String[] args) {
         System.out.println("hello world");
-
-        String JB = "je falat";
-
-        int cislo = 75;
-        String JB2 = "je falat2";
-        //nepis take sproste
+        //konec vypisovania
     }
 }
